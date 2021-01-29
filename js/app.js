9649fca3d896dd6c658d81889ff9c17e318f8a96
philrey12 <phil.raquinel@gmail.com>
2021-01-29 12:30:26 +0800

    Update app.js and styles.css

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ button.addEventListener('click', () => {
     let data = input.value;
 
     if (data.length > 0) {
-        let imgSrc = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${data}`;
+        let imgSrc = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${data}`;   // generate code with QR API
         qrImg.innerHTML = `<img src="${imgSrc}">`;
+    } else {
+        qrImg.innerHTML = '';
     }
 })
